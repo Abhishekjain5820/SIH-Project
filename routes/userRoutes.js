f23d5495b997ee322cba0b1dc91068734be16a39
Rahul Patel <rahulpatel43433@gmail.com>
2023-12-06 15:30:24 +0530

    delete functions

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,22 @@
-import express from 'express'
-const router=express.Router()
-import { registerController,loginController,userDetailsController } from '../controller/userController.js'
-import { requireSignIn } from '../middleware/authMiddleware.js'
+import express from "express";
+const router = express.Router();
+import {
+  registerController,
+  loginController,
+  userDetailsController,
+  updateController,
+  deleteStudentController,
+  deleteTeacherController,
+  deleteStudentByTeacherController,
+} from "../controller/userController.js";
+import { requireSignIn } from "../middleware/authMiddleware.js";
 
-router.post('/register',registerController)
-router.post('/login',loginController)
-router.get('/user-details/:userId', userDetailsController);
+router.post("/register", registerController);
+router.post("/login", loginController);
+router.get("/user-details/:userId", userDetailsController);
+router.post("/update-user/:userId", updateController);
+router.post("/delete-student/:userId", deleteStudentController);
+router.post("/delete-teacher/:userId", deleteTeacherController);
+router.post("/delete-studentby-teacher/:userId", deleteStudentByTeacherController);
 
 export default router;
